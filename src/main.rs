0589211610e7c89bba550bba5d715d6e614e9394
Alexander Kunkel <kunkel.alexander@protonmail.com>
2026-02-14 14:07:38 +0100

    disable warnings about unused functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-mod bus;
-mod cpu;
+#![allow(dead_code, unused, unused_variables, unused_imports, unused_comparisons)]
+pub mod bus;
+pub mod cpu;
+
 
 use crate::bus::Bus;
 use crate::cpu::Olc6502;
